40d2fe381dde9d3fb9b7bc5b17531889de45f831
Rudeus Greyrat <rudeusquagmire@gmail.com>
2024-11-29 03:03:52 -0500

    Added Credentials GRPC function

--- client/Credentials/credentials.py
+++ client/Credentials/credentials.py
@@ -1,17 +1,18 @@
+import json
 from grpcClient import GrpcClient
 
 GetCredentialsInstruction = "getCred"
 AddCredentialsInstruction = "addCred"
 
-def addCredentials(grpcClient: GrpcClient,TeamServerApi_pb2): 
+def addCredentials(grpcClient: GrpcClient,TeamServerApi_pb2, cred): 
     commandTeamServer = AddCredentialsInstruction
-    termCommand = TeamServerApi_pb2.TermCommand(cmd=commandTeamServer, data=b'{"hdddd": "g"}')
+    termCommand = TeamServerApi_pb2.TermCommand(cmd=commandTeamServer, data=cred.encode())
     resultTermCommand = grpcClient.sendTermCmd(termCommand)
     print(resultTermCommand.result)
 
 
 def getCredentials(grpcClient: GrpcClient,TeamServerApi_pb2): 
-    addCredentials(grpcClient, TeamServerApi_pb2)
+    addCredentials(grpcClient, TeamServerApi_pb2, '{"h": "b"}')
     commandTeamServer = GetCredentialsInstruction
     termCommand = TeamServerApi_pb2.TermCommand(cmd=commandTeamServer, data=b"")
     resultTermCommand = grpcClient.sendTermCmd(termCommand)
